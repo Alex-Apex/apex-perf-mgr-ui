329dada6a70ef04e9d312ed4b89647ce6d251c96
Alex-Apex <agomez@apexsystems.com>
2023-04-14 18:49:00 -0600

    Added all missing fields (no validations) for employees

--- src/components/EmployeeForm/EmployeeForm.js
+++ src/components/EmployeeForm/EmployeeForm.js
@@ -1,14 +1,26 @@
 // components/EmployeeForm.js
 import React, { useState, useContext, useEffect } from 'react';
 import { EmployeeContext } from '../../contexts/EmployeeContext';
+import EmployeeCard from '../EmployeeCard/EmployeeCard';
 
 function EmployeeForm({ employeeToEdit }) {
   const { addEmployee, editEmployee } = useContext(EmployeeContext);
   const [employee, setEmployee] = useState({
-    id: '',
-    name: '',
-    seniorityLevel: '',
-    // Add other fields as needed
+    id: '',        
+    name: '',        
+    seniorityLevel:'',        
+    practice: '',        
+    bhid:'',
+    status: '',        
+    firstMatch: '',        
+    secondaryMatch:'',        
+    mainSkillSet:'',
+    mostRecentProject: '',
+    projectRollOffDate: '',
+    weeksOnBench: '',        
+    resumeLink: '',
+    rejectionHistory:'',        
+    notes:'',    
   });
 
   useEffect(() => {
@@ -29,54 +41,45 @@ function EmployeeForm({ employeeToEdit }) {
     } else {
       addEmployee(employee);
       setEmployee({
-        id: '',
-        name: '',
-        seniorityLevel: '',
-        // Reset other fields as needed
+        id: '',        
+        name: '',        
+        seniorityLevel:'',        
+        practice: '',        
+        bhid:'',
+        status: '',        
+        firstMatch: '',        
+        secondaryMatch:'',        
+        mainSkillSet:'',
+        mostRecentProject: '',
+        projectRollOffDate: '',
+        weeksOnBench: '',        
+        resumeLink: '',
+        rejectionHistory:'',        
+        notes:'',   
       });
     }
   };
-
   return (
-    <form className="EmployeeForm" onSubmit={handleSubmit}>
-      <h2>{employeeToEdit ? 'Edit Employee' : 'Add Employee'}</h2>
-      <div>
-        <label htmlFor="id">ID:</label>
-        <input
-          type="text"
-          id="id"
-          name="id"
-          value={employee.id}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={employee.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="seniorityLevel">Seniority Level:</label>
-        <input
-          type="text"
-          id="seniorityLevel"
-          name="seniorityLevel"
-          value={employee.seniorityLevel}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      {/* Add other input fields as needed */}
-      <button type="submit">{employeeToEdit ? 'Update' : 'Add'}</button>
-    </form>
-  );
-}
+    <form onSubmit={handleSubmit}>
+        <EmployeeCard employee={employee}/>
+        <input type="text" name="id" value={employee.id} placeholder="Employee ID" onChange={handleChange}/>
+        <input type="text" name="name" value={employee.name} placeholder="Employee Name" onChange={handleChange}/>        
+        <input type="text" name="practice" value={employee.practice} placeholder="Practice" onChange={handleChange}/>
+        <input type="text" name="bhid" value={employee.bhid} placeholder="BHID" onChange={handleChange}/>
+        <input type="text" name="status" value={employee.status} placeholder="Status" onChange={handleChange}/>
+        <input type="text" name="firstMatch" value={employee.firstMatch} placeholder="First project Match" onChange={handleChange}/>
+        <input type="text" name="secondaryMatch" value={employee.secondaryMatch} placeholder="Second project match" onChange={handleChange}/>
+        <input type="text" name="seniorityLevel" value={employee.seniorityLevel} placeholder="Seniority Level" onChange={handleChange}/>
+        <input type="text" name="mainSkillSet" value={employee.mainSkillSet} placeholder="Main Skillset" onChange={handleChange}/>
+        <input type="text" name="mostRecentProject" value={employee.mostRecentProject} placeholder="Most Recent Project" onChange={handleChange}/>
+        <input type="text" name="projectRollOffDate" value={employee.projectRollOffDate} placeholder="project Roll Off date" onChange={handleChange}/>
+        <input type="text" name="weeksOnBench" value={employee.weeksOnBench} placeholder="Number of weeks on Bench" onChange={handleChange}/>
+        <input type="text" name="resumeLink" value={employee.resumeLink} placeholder="link to resume document" onChange={handleChange}/>
+        <input type="text" name="rejectionHistory" value={employee.rejectionHistory} placeholder="Rejection History" onChange={handleChange}/>
+        <input type="text" name="notes" value={employee.notes} placeholder="Notes" onChange={handleChange}/>
+        <button type="submit">{employeeToEdit ? 'Update' : 'Add'} Employee</button>
+      </form>
+    );
+  }
 
 export default EmployeeForm;
