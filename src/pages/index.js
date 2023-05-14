f8583524cab6de223d1b62db64fa523b57ff02e0
Alex-Apex <agomez@apexsystems.com>
2023-05-13 23:26:54 -0600

    Removed employee info from the index page. It should have only dashboard instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,16 @@
 // pages/index.js
 import React, { useState } from 'react';
-import EmployeeForm from '../components/EmployeeForm/EmployeeForm';
-import EmployeeList from '../components/EmployeeList/EmployeeList';
 import SidebarMenu from '../components/SidebarMenu/SidebarMenu';
 import ModalScreen from '../components/ModalScreen/ModalScreen';
 import Button from '../components/Button/Button';
 
 const HomePage = () => {
-  const[showModal, setShowModal] = useState(true); 
 
-  const toggleModalScreenVisibility = () => {    
-    setShowModal(!showModal);
-  }
-
-  const getModalButtons = () => {
-    return(
-      <div>
-        <Button onClick={toggleModalScreenVisibility} primary={false} label={`Cancel`} isSubmit={false}/>
-        <Button onClick={() => {console.log("BLAH");}} primary={true} label={`Add New Employee`} isSubmit={true}/>
-      </div>
-    );
-  }
-
-  const getAddNewEmployeeForm = () => {
-    return(
-      <EmployeeForm/>
-    );
-  }
 
   return (
     <div className="Home">
       <SidebarMenu/>      
-      <ModalScreen isOpen={showModal} 
-      children={getAddNewEmployeeForm()} title={`Adding a new employee`}
-      onClose={toggleModalScreenVisibility} // Remove the parentheses
-      buttons={getModalButtons()}/>
-      <EmployeeList />
+      <h1>My Dashboard should go here</h1>      
     </div>
   );
 }
